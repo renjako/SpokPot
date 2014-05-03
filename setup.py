00e5b3303791e0efcb527d018994705d976d6e2b
Aldo Aditya Alase <aldoalase@gmail.com>
2014-05-03 13:49:33 +0700

    push all made directory

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+        name = 'SpokPot',
+        packages = ['spokpot'],
+        version = '0.1',
+        description = 'Simple Python just OK Honeypot',
+        author = 'Aldo Aditya Alase',
+        author_email = 'aldoalase@gmail.com',
+        url = 'https://github.com/ararenja/SpokPot',
+#        download_url = '',
+        keywords = ['honeypot'],
+        classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Development Status :: Pre-alpha',
+        ],
+        long_description = 'Honeypot for final project that emulate RFI, LFI and some GHDB'
+)
