1786bda42bda66956e6f3a2c188355839d39b236
aldoalase <aldoalase@gmail.com>
2017-07-05 16:58:57 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
         packages = ['spokpot'],
         version = '0.1',
         description = 'Simple Python just OK Honeypot',
-        author = 'Aldo Alase',
-        author_email = 'aldoalase@gmail.com',
-        url = 'https://github.com/aldoalase/SpokPot',
+        author = '',
+        author_email = 'm',
+        url = '',
 #        download_url = '',
         keywords = ['honeypot'],
         classifiers = [
